cd58ece443ad572443f3deb5b2b2bc99028fd907
York Xiang <bombless@126.com>
2016-04-17 01:00:50 +0900

    Naive translation for autolink.h

---
+++ src/autolink_h.rs
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+const HOEDOWN_AUTOLINK_SHORT_DOMAINS: u32 = 1;
+
+
+
+
+pub use ::hoedown_autolink_is_safe;
+
+
+
+
+
+
+pub use ::hoedown_autolink__email;
+
+
+
+pub use ::hoedown_autolink__url;
+
+
+
+
+
+
+
